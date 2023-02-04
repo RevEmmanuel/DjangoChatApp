5434f725a122fef78d26e00f35cf3497d136dc3e
RevEmmanuel <adeolaae1@gmail.com>
2023-02-04 01:57:00 +0100

    final changes

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<str:room_name>/', views.room, name='room'),
-    path('checkview', views.check_view, name='check_view'),
-    path('send', views.send, name='send'),
+    path('<str:room_info>/<str:user_info>', views.room, name='room'),
+    path('check_view/', views.check_view, name='check_view'),
+    path('send/', views.send, name='send'),
+    path('get_messages/<str:room_name>/', views.get_messages, name='get_messages'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from chat.models import Room, Message
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 # Create your views here.
@@ -8,12 +8,11 @@ def home(request):
     return render(request, 'home.html')
 
 
-def room(request, room_name):
-    username = request.GET.get('username')
-    room_details = Room.objects.get(name=room_name)
+def room(request, room_info, user_info):
+    room_details, created = Room.objects.get_or_create(name=room_info)
     return render(request, 'room.html', {
-        'username': username,
-        'room': room,
+        'username': user_info,
+        'room': room_info,
         'room_details': room_details
     })
 
@@ -21,13 +20,13 @@ def room(request, room_name):
 def check_view(request):
     room_entity = request.POST['room_name']
     username = request.POST['username']
-
     if Room.objects.filter(name=room_entity).exists():
-        return redirect('/' + room_entity + '/?username=' + username)
+        return redirect('room', room_info=room_entity, user_info=username)
     else:
         new_room = Room.objects.create(name=room_entity)
         new_room.save()
-        return redirect('/' + room_entity + '/?username=' + username)
+        Room.save(new_room)
+        return redirect('room', room_info=room_entity, user_info=username)
 
 
 def send(request):
@@ -36,5 +35,12 @@ def send(request):
     room_id = request.POST['room_id']
 
     new_message = Message.objects.create(value=message, user=username, room=room_id)
+
     new_message.save()
     return HttpResponse('Message sent successfully')
+
+
+def get_messages(request, room_name):
+    # room_details = Room.objects.get(name=room_name)
+    messages = Message.objects.filter(room__icontains=room_name)
+    return JsonResponse({"messages": list(messages.values())})
